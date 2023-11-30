84619e73d5147e10951106120f8c31cb90fd1db4
micheldutralepine <133803485+micheldutralepine@users.noreply.github.com>
2023-11-29 22:26:57 -0300

    Add files via upload
    projeto arrumado

--- carteirinha.py
+++ carteirinha.py
@@ -1,32 +1,37 @@
-
-
 def cadastrar_usuario(usuarios):
     nome = input("Digite o seu nome: ")
     idade = input("Digite a sua idade: ")
+
+    if not idade.isnumeric():
+        print("Idade inválida. Digite um número.")
+        return
 
     usuario = [nome, idade, []]
 
     usuarios.append(usuario)
     print("Usuário cadastrado com sucesso!")
+    return usuarios
 
 
 def cadastrar_vacina(usuarios):
     if not usuarios:
         print("Nenhum usuário cadastrado, cadastre um primeiro.")
-        return
+        return usuarios
 
     nome_usuario = input("Digite o nome do usuário: ")
-    ususario_encontrado = False
+    usuario_encontrado = False
 
     for usuario in usuarios:
         if usuario[0].lower() == nome_usuario.lower():
             vacina = input("Digite o nome da vacina: ")
             usuario[2].append(vacina)
-            ususario_encontrado = True
+            usuario_encontrado = True
             print("Vacinação cadastrada com sucesso!")
 
-    if not ususario_encontrado:
+    if not usuario_encontrado:
         print("Usuário não encontrado.")
+
+    return usuarios
 
 
 def mostrar_carteira(usuarios):
@@ -34,26 +39,28 @@ def mostrar_carteira(usuarios):
         print('Nenhum usuário cadastrado, cadastre um primeiro: ')
         return
 
-    nome_usuario = input('digite o nome do paciente:')
-    usuario_encotrado = False
+    nome_usuario = input('Digite o nome do usuário: ')
+    usuario_encontrado = False
     for usuario in usuarios:
         if usuario[0].lower() == nome_usuario.lower():
             print(f'Nome: {usuario[0]}')
-            print(f'idade: {usuario[1]}')
+            print(f'Idade: {usuario[1]}')
             print('Vacinas: ')
             for vacina in usuario[2]:
                 print(f' - {vacina}')
-            usuario_encotrado = True
+            usuario_encontrado = True
 
-    if not usuario_encotrado:
-        print('Usuário não encontrado. ')
+    if not usuario_encontrado:
+        print('Usuário não encontrado.')
+
+    return usuarios
 
 
 def main():
     usuarios = []
 
     while True:
-        print('------------- Carterinha de vacinação -------------')
+        print('------------- Carteirinha de vacinação -------------')
         print("1. Cadastrar novo Usuário")
         print("2. Cadastrar vacinação")
         print("3. Exibir carteirinha de Usuário")
@@ -62,13 +69,13 @@ def main():
         escolha = input("Escolha uma opção (1/2/3/4): ")
 
         if escolha == "1":
-            cadastrar_usuario(usuarios)
+            usuarios = cadastrar_usuario(usuarios)
 
         elif escolha == "2":
-            cadastrar_vacina(usuarios)
+            usuarios = cadastrar_vacina(usuarios)
 
         elif escolha == "3":
-            mostrar_carteira(usuarios)
+            usuarios = mostrar_carteira(usuarios)
 
         elif escolha == "4":
             print("Saindo do programa. Até mais!")
